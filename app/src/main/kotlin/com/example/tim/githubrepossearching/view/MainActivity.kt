41be453150ba5e9f26889515d4b369c4c5d09d77
panzhiev <panzhiev.timur@gmail.com>
2017-08-08 19:43:33 +0300

    bug fixed 2

--- app/src/main/kotlin/com/example/tim/githubrepossearching/view/MainActivity.kt
+++ app/src/main/kotlin/com/example/tim/githubrepossearching/view/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.tim.githubrepossearching.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.example.tim.githubrepossearching.R
+import com.tim.kotlinjakewharton.view.activities.IActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IActivity {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun setData(list: ArrayList<String>) {
+
+    }
+
+    override fun showException(e: Throwable) {
+        Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+    }
+
+//    override fun onStop() {
+//        mainPresenter.onUnsubscribe()
+//        super.onStop()
+//    }
 }
